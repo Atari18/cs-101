db9161a9741bbb324537cd659cfe3fbbc739377c
Atari18 <AppleAtari@proton.me>
2026-01-05 20:30:43 +0000

    chore: add documentation to utils.py

--- src/ticket_machine_logic/utils.py
+++ src/ticket_machine_logic/utils.py
@@ -1,3 +1,4 @@
+# Importing Libraries
 import time
 import sys
 import re
@@ -10,13 +11,21 @@ BLUE = "\033[94m"
 STRIKETHROUGH = "\033[9m"
 RESET = "\033[0m"
 
-def format_text(text, width=40, decorated=False):
-    # Strip ANSI codes to calculate visible length
+def format_text(text: str, width: int=40, decorated: bool=False):
+    """
+    Center's text with the width of 40 and adds a border if specified.
+    Args:
+        text (str): The text to be formatted.
+        width (int): The width of the formatted text.
+        decorated (bool): Whether to add a border around the text.
+
+    Returns:
+        The formatted text.
+    """
     visible_text = re.sub(r'\033\[[0-9;]*m', '', text)
     padding = max(0, (width - len(visible_text)) // 2)
     
     formatted_text = f"{' ' * padding}{text}"
-
     if decorated:
         border = "=" * width
         formatted_text = f"\n{border}\n{formatted_text}\n{border}"
@@ -24,6 +33,19 @@ def format_text(text, width=40, decorated=False):
     return formatted_text
 
 def check_input(input_str, input_range):
+    """
+    Checks whether a given input string is a valid representation of a value
+    within a specified range or collection.
+
+    Args:
+        input_str (str): The input string to check.
+        input_range (Union[range, Iterable]): The range or collection to validate
+            the input string against.
+
+    Returns:
+        bool: True if the input string corresponds to a valid value within the
+        specified range or collection, False otherwise.
+    """
     if not input_str.isdigit():
         return False
     
@@ -34,6 +56,15 @@ def check_input(input_str, input_range):
     return input_str in map(str, input_range)
 
 def print_cbc(text, color=None, delay=0.01):
+    """
+    The print_character_by_character function prints text with color and delay for a more terminal-based feel,
+    handling ANSI escape sequences.
+
+    Args:
+        text (str): The text to be printed.
+        color (str, optional): ANSI escape sequence for color. Defaults to None.
+        delay (float, optional): Delay between characters in seconds. Defaults to 0.01.
+    """
     if text is None:
         return
 
---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+variable = 1
+
+def test_function():
+    if variable.isdigit():
+        print("Variable is a digit")
+    else:
+        print("Variable is not a digit")
+
+test_function()
